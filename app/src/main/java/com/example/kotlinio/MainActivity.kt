f4aba8ae5c9e8477b59f49b60e7c8f6bbac760ea
M S I <ghani.zumar.ft21@mail.umy.ac.id>
2023-10-27 09:21:27 +0700

    menambahkan text create your account

--- app/src/main/java/com/example/kotlinio/MainActivity.kt
+++ app/src/main/java/com/example/kotlinio/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -34,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,6 +60,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun TampilanLayout(
     modifier: Modifier =Modifier
@@ -73,6 +74,7 @@ fun TampilanLayout(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(20.dp)
         ){
+            Text(text = "Create Your Account", fontSize = 25.sp, fontWeight = FontWeight.Bold)
             TampilanForm()
         }
     }
@@ -189,15 +191,15 @@ fun TextHasil(namanya:String, telponya:String, alamatnya:String, jenisnya:String
     ) {
         Text(text = "Nama : " + namanya,
             modifier = Modifier
-                .padding(horizontal = 10.dp, vertical = 4.dp)
+                    .padding(horizontal = 10.dp, vertical = 4.dp)
         )
         Text(text = "Telepon : " + telponya,
             modifier = Modifier
-                .padding(horizontal = 10.dp, vertical = 5.dp)
+                    .padding(horizontal = 10.dp, vertical = 5.dp)
         )
         Text(text = "Alamat : " + alamatnya,
             modifier = Modifier
-                .padding(horizontal = 10.dp, vertical = 5.dp)
+                    .padding(horizontal = 10.dp, vertical = 5.dp)
         )
         Text(text = "Jenis Kelamin : " + jenisnya,
                 modifier = Modifier
